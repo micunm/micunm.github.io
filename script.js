a15f53178b8d396e66fdea35cb39b9d0aea8e864
micunm <micun@o2.pl>
2018-08-31 05:53:57 +0200

    Tasks Application github pages updt2 task 19.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 //  const apiRoot = 'http://localhost:8080/v1/task/';
-  const apiRoot = 'http://gentle-spire-20140.herokuapp.com/v1/task/';
+  const apiRoot = 'https://gentle-spire-20140.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
